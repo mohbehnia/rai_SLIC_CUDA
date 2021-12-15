90f0f439b59c3ef71fac3eec1f3b066e23103a59
mohbehnia <behmohmay@gmail.com>
2021-12-15 17:48:01 -0600

    minor updates

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ using namespace cv;
 int main() {
  
 	// VideoCapture cap("/home/jimmy/ece508/data/waterfall.avi");
-	cout << "FIRST0" << endl;
+
 
 	// Parameters
 	int wc = 35;
@@ -18,31 +18,41 @@ int main() {
 	SlicCuda::InitType initType = SlicCuda::SLIC_SIZE;
 
 	//start segmentation
+
+	auto t30 = std::chrono::high_resolution_clock::now();
+
 	Mat frame;
-	frame = imread("/src/temple.jpg", IMREAD_COLOR);
+	frame = imread("/src/coco.jpeg", IMREAD_COLOR);
 	int diamSpx = sqrt(frame.rows*frame.cols/10000); //want about 10
 	SlicCuda oSlicCuda;
 	oSlicCuda.initialize(frame, diamSpx, initType, wc, nIteration);
 	Mat labels;
-	auto t0 = std::chrono::high_resolution_clock::now();
+	auto t00 = std::chrono::high_resolution_clock::now();
 	oSlicCuda.segment(frame);
-	auto t1 = std::chrono::high_resolution_clock::now();
-	double time = std::chrono::duration<double>(t1-t0).count() ;
-	cout << "Frame " << frame.size() << "Segment Time: "<< time <<"s"<<endl;
+	auto t01 = std::chrono::high_resolution_clock::now();
+	double time0 = std::chrono::duration<double>(t01-t00).count() ;
+	cout << "Frame " << frame.size() << "Segment Time: "<< time0 <<"s"<<endl;
 
 	oSlicCuda.enforceConnectivity();
-	cout << "FIRST6" << endl;
 
 	labels = oSlicCuda.getLabels();
 	auto data = labels.data;
-	cout << "FIRST7" << endl;
+
 	// SlicCuda::displayBound(frame, (float*)labels.data, Scalar(0, 0, 0));
+	auto t20 = std::chrono::high_resolution_clock::now();
 	SlicCuda::displayPoint1(frame, (float*)labels.data, Scalar(0, 0, 0));
-	cout << "FIRST8" << endl;
+	auto t21 = std::chrono::high_resolution_clock::now();
+	double time2 = std::chrono::duration<double>(t21-t20).count() ;
+
+	cout << "Triangle Time: "<< time2 <<"s"<<endl;
 
 	bool success = imwrite("/build/out.jpg", frame);
 
 	cout << "WRITE " << success << endl;
+
+	auto t31 = std::chrono::high_resolution_clock::now();
+	double time3 = std::chrono::duration<double>(t31-t30).count() ;
+	cout << "Total Time: "<< time3 << "s"<< endl;
 
     return 0;
 }
